38a9954930f1b4b98bf36cd382ce125bda7bf032
biglight <biglight.huang@gmail.com>
2021-01-08 20:53:31 +0800

    升级框架版本

--- controllers/index.go
+++ controllers/index.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
+	"github.com/beego/beego/server/web"
 )
 
 type MainController struct {
-	beego.Controller
+	web.Controller
 }
 
 func (c *MainController) Index() {
