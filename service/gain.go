e687d05f1dd23a9c5f826d7c5243f004c0a530dd
biglight <biglight.huang@gmail.com>
2021-01-11 17:46:07 +0800

    修改源

--- service/gain.go
+++ service/gain.go
@@ -26,8 +26,8 @@ func BookTwoLists(domain string) {
 	c := colly.NewCollector()
 
 	// Find and visit all links
-	c.OnXML("//body", func(e *colly.XMLElement) {
-		lastLink := e.ChildText("//font[contains(text(),'第1页，共22页')]")
+	c.OnXML("//body/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/span[1]", func(e *colly.XMLElement) {
+		lastLink := e.ChildText("//font")
 		spew.Dump(lastLink, domain)
 		os.Exit(1)
 		allNum, _ := strconv.Atoi(lastLink)
