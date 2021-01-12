8a5fdf7e57132b763c9025b631bb15e1b0fc5ba9
biglight <biglight.huang@gmail.com>
2021-01-12 11:30:52 +0800

    修改源

--- service/gain.go
+++ service/gain.go
@@ -137,7 +137,7 @@ func twoPickLinks(d *colly.Collector) {
 	d.OnXML("//body/div[6]/div[1]/div[2]/div[1]/div[5]/div", func(f *colly.XMLElement) {
 		link := f.ChildText("//div[1]/a[1]/@href")
 		title := f.ChildText("//div[1]/div[1]/h3[1]/a/text()")
-		lastCharpter := f.ChildText("//div[1]/div[1]/a")
+		lastCharpter := f.ChildText("./div[1]/div[1]/a")
 		t1 := f.ChildText("/div[1]/div[1]/small[1]/a[1]/text()")
 		t2 := ""
 		if link != "" {
